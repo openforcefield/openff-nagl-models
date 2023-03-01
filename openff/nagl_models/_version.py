d1341a5e856122af291ca2ee04db2f4e08343b71
Lily Wang <31115101+lilyminium@users.noreply.github.com>
2023-03-01 05:51:38 -0800

    re-install versioneer

--- openff/nagl_models/_version.py
+++ openff/nagl_models/_version.py
@@ -46,7 +46,7 @@ def get_config():
     cfg.style = "pep440"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "None"
-    cfg.versionfile_source = "openff_nagl_models/_version.py"
+    cfg.versionfile_source = "openff/nagl_models/_version.py"
     cfg.verbose = False
     return cfg
 
