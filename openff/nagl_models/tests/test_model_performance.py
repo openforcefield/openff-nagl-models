bc31775d58781f88bfc1e0992087e5fd4d2e63d2
Lily Wang <lily@macaque.local>
2023-08-16 15:07:36 +1000

    add tests

---
+++ openff/nagl_models/tests/test_model_performance.py
@@ -0,0 +1,31 @@
+import pytest
+import numpy as np
+
+pytest.importorskip("openff.nagl")
+
+
+@pytest.mark.parametrize(
+    "model_file, expected_charges",
+    [
+        (
+            "openff-gnn-am1bcc-0.0.1-alpha.1.pt",
+            [-0.119291  ,  0.12953702, -0.60555816,  0.04394022, 
+             0.04394018, 0.04394022,  0.03412103,  0.034121  ,
+               0.39524958]
+        )
+    ]
+)
+def test_models_ethanol(model_file, expected_charges):
+    from openff.nagl.nn._models import GNNModel
+    from openff.toolkit import Molecule
+    from openff.nagl_models import validate_nagl_model_path
+
+    model_path = validate_nagl_model_path(model_file)
+    model = GNNModel.load(model_path, eval_mode=True)
+    ethanol = Molecule.from_smiles("CCO")
+    charges = model.compute_property(
+        ethanol,
+        readout_name="am1bcc-charges",
+        as_numpy=True
+    )
+    np.testing.assert_allclose(charges, expected_charges, atol=1e-5)
