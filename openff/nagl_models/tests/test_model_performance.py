bb29abdf9847bca59fe7ede3a31ee904e77fcb75
Lily Wang <lilywang@lilac-ln02.hpc.private>
2023-08-29 00:25:47 -0400

    update model version and test

--- openff/nagl_models/tests/test_model_performance.py
+++ openff/nagl_models/tests/test_model_performance.py
@@ -15,9 +15,9 @@ pytest.importorskip("openff.nagl")
         ),
         (
             "openff-gnn-am1bcc-0.1.0-rc.1.pt",
-            [-0.119291  ,  0.12953702, -0.60555816,  0.04394022, 
-             0.04394018, 0.04394022,  0.03412103,  0.034121  ,
-               0.39524958]
+            [-0.101634,  0.133269, -0.605404, 
+             0.043411,  0.043411,  0.043411,
+             0.019863,  0.019863,  0.40381 ]
         )
     ]
 )
@@ -31,7 +31,7 @@ def test_models_ethanol(model_file, expected_charges):
     ethanol = Molecule.from_smiles("CCO")
     charges = model.compute_property(
         ethanol,
-        readout_name="am1bcc-charges",
+        readout_name="am1bcc_charges",
         as_numpy=True
     )
     np.testing.assert_allclose(charges, expected_charges, atol=1e-5)
