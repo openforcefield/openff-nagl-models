2d769684a8f9331b0123ff328b4c29f4a70f846f
Lily Wang <lily@mdanalysis.org>
2024-02-09 15:08:42 +1100

    add get_models_by_top to top level import

--- openff/nagl_models/__init__.py
+++ openff/nagl_models/__init__.py
@@ -4,7 +4,13 @@ Models used with NAGL
 """
 
 # Add imports here
-from .openff_nagl_models import get_nagl_model_dirs_paths, load_nagl_model_directory_entry_points, validate_nagl_model_path, list_available_nagl_models
+from .openff_nagl_models import (
+    get_nagl_model_dirs_paths,
+    load_nagl_model_directory_entry_points,
+    validate_nagl_model_path,
+    list_available_nagl_models,
+    get_models_by_type
+)
 
 # Handle versioneer
 from . import _version
