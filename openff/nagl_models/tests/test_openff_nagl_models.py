a7c24d7305ddbeb5b188a0b7b16263babb07609d
Matt Thompson <matt.thompson@openforcefield.org>
2025-03-20 16:19:34 -0500

    Do not use old `pkg_resources` in test (#46)

--- openff/nagl_models/tests/test_openff_nagl_models.py
+++ openff/nagl_models/tests/test_openff_nagl_models.py
@@ -4,7 +4,7 @@ Unit and regression test for the openff_nagl_models package.
 
 import os
 import importlib.resources
-
+from importlib_metadata import entry_points
 
 import pytest
 from openff.nagl_models import validate_nagl_model_path, list_available_nagl_models
@@ -51,8 +51,9 @@ def test_list_models():
 
 
 def test_entry_points():
-    from pkg_resources import iter_entry_points
-    for entry_point in iter_entry_points(group='openforcefield.nagl_model_directory'):
+    for entry_point in entry_points().select(
+        group='openforcefield.nagl_model_directory',
+    ):
         paths = entry_point.load()()
         for path in paths:
             assert os.path.exists(path)
