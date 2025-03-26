b208a39dc727e2db74bb039865d699f48e3dfe98
Matthew W. Thompson <mattwthompson@protonmail.com>
2025-03-26 12:39:35 -0500

    Add to module-level import

--- openff/nagl_models/__init__.py
+++ openff/nagl_models/__init__.py
@@ -1,9 +1,8 @@
 """
-Open NAGL Models
+OpenFF NAGL Models
 Models used with NAGL
 """
 
-# Add imports here
 from .openff_nagl_models import (
     get_nagl_model_dirs_paths,
     load_nagl_model_directory_entry_points,
@@ -11,7 +10,17 @@ from .openff_nagl_models import (
     list_available_nagl_models,
     get_models_by_type
 )
+from openff.nagl_models._dynamic_fetch import get_model
 
 # Handle versioneer
 from . import _version
 __version__ = _version.get_versions()['version']
+
+__all__ = (
+    "get_nagl_model_dirs_paths",
+    "load_nagl_model_directory_entry_points",
+    "validate_nagl_model_path",
+    "list_available_nagl_models",
+    "get_models_by_type",
+    "get_model",
+)
